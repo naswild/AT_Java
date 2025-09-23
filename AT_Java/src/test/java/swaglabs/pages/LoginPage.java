c49db402b9719069552a2f1fbb2cc4273ecc4754
nastya <nastyaev4uk@gmail.com>
2025-09-23 20:56:28 +0300

    Added BasePage and ProductsPage Made tests for login (without password and without username)

--- AT_Java/src/test/java/swaglabs/pages/LoginPage.java
+++ AT_Java/src/test/java/swaglabs/pages/LoginPage.java
@@ -2,26 +2,35 @@ package swaglabs.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class LoginPage {
+public class LoginPage extends BasePage {
 
     private static final By USERNAME_INPUT = By.name("user-name");
     private static final By PASSWORD_INPUT = By.name("password");
     private static final By LOGIN_BTN = By.name("login-button");
+    private static final By ERROR = By.xpath("//*[@data-test='error']");
 
-    private final WebDriver browser;
-
-    public LoginPage(WebDriver browser) {
-        this.browser = browser;
+    public LoginPage(WebDriver driver) {
+        super(driver);
     }
 
     public void open() {
-        browser.get("https://www.saucedemo.com/");
+        driver.get(BASE_URL);
+    }
+
+    public void open(String path) {
+        driver.get(BASE_URL + path);
     }
 
     public void login(String username, String password) {
-        browser.findElement(USERNAME_INPUT).sendKeys(username);
-        browser.findElement(PASSWORD_INPUT).sendKeys(password);
-        browser.findElement(LOGIN_BTN).click();
+        driver.findElement(USERNAME_INPUT).sendKeys(username);
+        driver.findElement(PASSWORD_INPUT).sendKeys(password);
+        driver.findElement(LOGIN_BTN).click();
+    }
+
+    public String checkErrorMsg() {
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(ERROR)).getText();
     }
 }
--- AT_Java/src/test/java/swaglabs/tests/LoginTest.java
+++ AT_Java/src/test/java/swaglabs/tests/LoginTest.java
@@ -1,6 +1,5 @@
 package swaglabs.tests;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import swaglabs.parent.BaseTest;
 
@@ -9,23 +8,33 @@ import static org.testng.Assert.assertEquals;
 
 public class LoginTest extends BaseTest {
 
-    @Test
+    @Test(description = "Проверка корректной авторизации")
     public void checkLoginWithCorrectCredentials() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-
-        boolean isProductDisplayed = browser.findElement(By.xpath("//*[@data-test='title']"))
-                .isDisplayed();
-
-        assertTrue(isProductDisplayed);
+        assertTrue(productsPage.isTitlePresent());
+        assertEquals(productsPage.getTitle(), "Products",
+                "Название заголовка не соответствует ожидаемому");
     }
 
     @Test
     public void checkLoginWithLockedOutUser() {
         loginPage.open();
         loginPage.login("locked_out_user", "secret_sauce");
+        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Sorry, this user has been locked out.");
+    }
 
-        String errorMsg = browser.findElement(By.xpath("//*[@data-test='error']")).getText();
-        assertEquals(errorMsg, "Epic sadface: Sorry, this user has been locked out.");
+    @Test(description = "Проверка сообщения об ошибке при пустом поле username")
+    public void checkLoginWithoutUsername() {
+        loginPage.open();
+        loginPage.login("", "secret_sauce");
+        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Username is required");
+    }
+
+    @Test(description = "Проверка сообщения об ошибке при пустом поле password")
+    public void checkLoginWithoutPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Password is required");
     }
 }

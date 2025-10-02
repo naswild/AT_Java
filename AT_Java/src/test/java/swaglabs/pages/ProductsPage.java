3e45f4541d61dc2309777d1b46918d97744b4266
nastya <nastyaev4uk@gmail.com>
2025-10-03 00:47:23 +0300

    Lesson 8 Allure reports

--- AT_Java/src/test/java/swaglabs/pages/ProductsPage.java
+++ AT_Java/src/test/java/swaglabs/pages/ProductsPage.java
@@ -1,5 +1,6 @@
 package swaglabs.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,15 +20,18 @@ public class ProductsPage extends BasePage {
         super(driver);
     }
 
+    @Step("Check page title text")
     public String getTitle() {
         return driver.findElement(title).getText();
     }
 
+    @Step("Check page title is displayed")
     public boolean isTitlePresent() {
         return wait.until(
                 ExpectedConditions.visibilityOfElementLocated(title2)).isDisplayed();
     }
 
+    @Step("Add product {productName} to cart")
     public void addToCart(String productName) {
         By addToCart = By.xpath(ADD_TO_CART_BTN_PATTERN.formatted(productName));
         driver.findElement(addToCart).click();
@@ -37,17 +41,20 @@ public class ProductsPage extends BasePage {
         driver.findElements(ADD_TO_CART_BTN).get(productIndex).click();
     }
 
+    @Step("Add {productNumber} products to cart")
     public void addProductsToCart(int productNumber) {
         for (int i = 0; i < productNumber; i++) {
             this.addToCart(0);
         }
     }
 
+    @Step("Get cart icon counter value")
     public String getNumberCart() {
         return wait.until(
                 ExpectedConditions.visibilityOfElementLocated(CART_BADGE)).getText();
     }
 
+    @Step("Open cart")
     public void openCart() {
         driver.findElement(CART_LINK).click();
     }
--- AT_Java/src/test/java/swaglabs/tests/LoginTest.java
+++ AT_Java/src/test/java/swaglabs/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package swaglabs.tests;
 
+import io.qameta.allure.*;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import swaglabs.parent.BaseTest;
@@ -12,25 +13,32 @@ import static swaglabs.user.UserFactory.withLockedOutUserPermission;
 
 public class LoginTest extends BaseTest {
 
-    @Test(description = "Проверка корректной авторизации")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Anastasiia Evchuk anastasiiaevchuk@gmail.com")
+    @TmsLink("helloworld")
+    @Issue("Homeworks")
+    @Test(description = "Check correct login")
     public void checkLoginWithCorrectCredentials() {
         loginPage.open();
         loginPage.login(withClientPermission());
         assertTrue(productsPage.isTitlePresent());
         assertEquals(productsPage.getTitle(), "Products",
-                "Название заголовка не соответствует ожидаемому");
+                "Title doesn't match expected value");
     }
 
     @DataProvider
     public Object[][] loginData() {
-        return new Object[][] {
-                { withLockedOutUserPermission(), "Epic sadface: Sorry, this user has been locked out." },
-                { new User("", "secret_sauce"), "Epic sadface: Username is required" },
-                { new User("standard_user", ""), "Epic sadface: Password is required" }
+        return new Object[][]{
+                {withLockedOutUserPermission(), "Epic sadface: Sorry, this user has been locked out."},
+                {new User("", "secret_sauce"), "Epic sadface: Username is required"},
+                {new User("standard_user", ""), "Epic sadface: Password is required"}
         };
     }
 
-    @Test(description = "Проверка сообщения об ошибке", dataProvider = "loginData")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Anastasiia Evchuk anastasiiaevchuk@gmail.com")
+    @TmsLink("helloworld")
+    @Test(description = "Check error message", dataProvider = "loginData")
     public void checkIncorrectLogin(User user, String errorMsg) {
         loginPage.open();
         loginPage.login(user);
